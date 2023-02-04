d8cc506c986d1d9daaf1e0b18503b64aa6799449
Janani-Balasooriya <jananidev7@gmail.com>
2023-02-05 01:25:36 +0530

    card component done

---
+++ src/components/SimpleCard.js
@@ -0,0 +1,18 @@
+import "./SimpleCard.css";
+
+function SimpleCard() {
+  return (
+    <div className="card-container">
+      <h2>Introduction</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <button>View More</button>
+    </div>
+  );
+}
+
+export default SimpleCard;
