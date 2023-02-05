7bfe10c9da3d8c8df6d49e3db97bea9e09b32ef1
Janani-Balasooriya <jananidev7@gmail.com>
2023-02-06 00:16:23 +0530

    More styling done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Product from "./components/Product";
 import SimpleCard from "./components/SimpleCard";
 
 function App() {
   return (
     <>
-      <SimpleCard />
       <Navbar />
+      <SimpleCard />
     </>
   );
 }
